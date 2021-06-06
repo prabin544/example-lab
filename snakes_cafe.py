edff9fce3033e6f2afced93bf056b89e13897d38
prabin544 <42320486+prabin544@users.noreply.github.com>
2021-06-05 22:20:43 -0500

    Lab01 commit

---
+++ snakes_cafe.py
@@ -0,0 +1,41 @@
+print('**************************************')
+print('**    Welcome to the Snakes Cafe!   **')
+print('**    Please see our menu below.    **')
+print('** To quit at any time, type "quit" **')
+print('**************************************')
+
+print(
+    '''
+    Appetizers
+    ----------
+    Wings
+    Cookies
+    Spring Rolls
+
+    Entrees
+    -------
+    Salmon
+    Steak
+    Meat Tornado
+    A Literal Garden
+
+    Desserts
+    --------
+    Ice Cream
+    Cake
+    Pie
+
+    Drinks
+    ------
+    Coffee
+    Tea
+    Unicorn Tears
+    '''
+    )
+print('What would you like to order?')
+order=input('>')
+x = 0
+while order != 'quit':
+    x = x + 1 
+    print(f'** {x} order of {order} have been added to your meal **')
+    order=input('>')    
